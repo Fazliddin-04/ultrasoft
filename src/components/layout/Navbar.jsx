bae55110cd12c3f4eeb86d7f207c8120d346bf96
Fazliddin <78717334+Fazliddin-04@users.noreply.github.com>
2022-01-16 16:19:31 +0500

    Update Navbar.jsx

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -59,86 +59,84 @@ function Navbar({ title }) {
   themeChange()
 
   return (
-
-        <div className="w-full navbar bg-base-300 fixed top-0 left-0 z-50">
-          <div className="flex-none lg:hidden">
-            <label htmlFor="my-drawer-3" className="btn btn-square btn-ghost">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                className="inline-block w-6 h-6 stroke-current"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h16M4 18h16"
-                ></path>
-              </svg>
-            </label>
-          </div>
-          <div className="flex-1 px-2 mx-2">
-            <Link
-              to="/"
-              className="text-2xl sm:text-3xl lg:text-4xl font-extrabold"
-            >
-              <span className="bg-clip-text uppercase text-transparent bg-gradient-to-r from-pink-500 to-purple-500">
-                {title}
-              </span>
+    <div className="w-full navbar bg-neutral text-neutral-content fixed top-0 left-0 z-50">
+      <div className="flex-none lg:hidden">
+        <label htmlFor="my-drawer-3" className="btn btn-square btn-ghost">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            className="inline-block w-6 h-6 stroke-current"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h16M4 18h16"
+            ></path>
+          </svg>
+        </label>
+      </div>
+      <div className="flex-1 px-2 mx-2">
+        <Link
+          to="/"
+          className="flex items-start text-2xl sm:text-3xl lg:text-4xl font-extrabold uppercase"
+        >
+          <span className="text-primary">{title}</span>
+          <span className="ml-1 rounded-md text-sm px-1 text-white h-max bg-primary">
+            .uz
+          </span>
+        </Link>
+      </div>
+      <div className="flex-none hidden lg:block">
+        <ul className="menu horizontal">
+          <li>
+            <Link to="/" className="btn btn-ghost rounded-none">
+              <div className="inline-block w-5 mr-2">
+                <i className="far fa-home"></i>
+              </div>
+              Bosh sahifa
             </Link>
-          </div>
-          <div className="flex-none hidden lg:block">
-            <ul className="menu horizontal">
-              <li>
-                <Link to="/" className="btn btn-ghost rounded-none">
-                  <div className="inline-block w-5 mr-2">
-                    <i className="far fa-home"></i>
-                  </div>
-                  Bosh sahifa
-                </Link>
-              </li>
-              <li>
-                <Link to="/files" className="btn btn-ghost rounded-none">
-                  <div className="inline-block w-5 mr-2">
-                    <i className="far fa-folder"></i>
-                  </div>
-                  Fayllarim
-                </Link>
-              </li>
-              <li>
-                <select
-                  data-choose-theme
-                  className="appearance-none btn btn-ghost rounded-none mr-5 px-1"
-                >
-                  <option value="light">🌝 Yorqin</option>
-                  <option value="dark">🌚 Tun</option>
-                  <option value="cupcake">🧁 Cupcake</option>
-                  <option value="halloween">🎃 Halloween</option>
-                  <option value="bumblebee">🐝 Bumblebee</option>
-                  <option value="synthwave">🌃 Synthwave</option>
-                  <option value="cyberpunk">🤖 Cyberpunk</option>
-                  <option value="wireframe">📝 Qo'lyozma</option>
-                </select>
-              </li>
-              <li>
-                <button
-                  className="btn btn-primary"
-                  onClick={() => navigate('/profile')}
-                >
-                  shaxsiy Hisob
-                </button>
-              </li>
-            </ul>
-          </div>
-        </div>
-      
-
+          </li>
+          <li>
+            <Link to="/files" className="btn btn-ghost rounded-none">
+              <div className="inline-block w-5 mr-2">
+                <i className="far fa-folder"></i>
+              </div>
+              Fayllarim
+            </Link>
+          </li>
+          <li>
+            <select
+              data-choose-theme
+              className="appearance-none btn btn-ghost rounded-none mr-5 px-1"
+            >
+              <option value="light">🌝 Yorqin</option>
+              <option value="dark">🌚 Tun</option>
+              <option value="cupcake">🧁 Cupcake</option>
+              <option value="halloween">🎃 Halloween</option>
+              <option value="bumblebee">🐝 Bumblebee</option>
+              <option value="synthwave">🌃 Synthwave</option>
+              <option value="cyberpunk">🤖 Cyberpunk</option>
+              <option value="wireframe">📝 Qo'lyozma</option>
+            </select>
+          </li>
+          <li>
+            <button
+              className="btn btn-primary"
+              onClick={() => navigate('/profile')}
+            >
+              shaxsiy Hisob
+            </button>
+          </li>
+        </ul>
+      </div>
+    </div>
   )
 }
 
 Navbar.defaultProps = {
-  title: 'Ultrasoft.uz',
+  title: 'Ultrasoft',
 }
 
 Navbar.propTypes = {
