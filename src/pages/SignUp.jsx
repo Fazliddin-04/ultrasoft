b86d8a6d93512a36eded28a9659ea2fc264aaae6
Fazliddin <78717334+Fazliddin-04@users.noreply.github.com>
2022-02-11 18:10:56 +0500

    Update SignUp.jsx

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -97,76 +97,74 @@ function SignUp() {
   })
 
   return (
-    <>
-      <div className="mx-auto bg-base-300 w-max max-w-full py-5 px-14 sm:my-10 sm:rounded">
-        <p className="text-3xl sm:text-4xl font-bold capitalize max-w-xs text-center my-5">
-          <span className="text-accent">Xush kelibsiz!</span>
-        </p>
-        <div className="flex justify-center mt-10 mb-5">
-          <OAuth />
-        </div>
-        <form className="form-control" onSubmit={onSubmit}>
-          <div className="form-control-magic">
-            <input
-              className="input input-ghost w-full"
-              type="name"
-              id="name"
-              value={name}
-              onChange={onChange}
-              required
-            />
-            <label className="label block" ref={ref1}>
-              Ism
-            </label>
-          </div>
-          <div className="form-control-magic">
-            <input
-              className="input input-ghost w-full"
-              type="email"
-              id="email"
-              value={email}
-              onChange={onChange}
-              required
-            />
-            <label className="label block" ref={ref2}>
-              Email
-            </label>
-          </div>
-          <div className="form-control-magic flex items-center justify-center">
-            <input
-              className="input input-ghost w-full"
-              type={showPassword ? 'text' : 'password'}
-              id="password"
-              value={password}
-              onChange={onChange}
-              required
-            />
-            <label className="label block" ref={ref3}>
-              <span className="label-text text-xl">Parol</span>
-            </label>
-            <button
-              type="button"
-              className="btn btn-outline btn-square btn-sm absolute right-2"
-              onClick={() => setShowPassword((prevState) => !prevState)}
-            >
-              <i className="far fa-eye"></i>
-            </button>
-          </div>
-
-          <button className="btn btn-block btn-primary mt-5 gap-3">
-            Ro'yxatdan o'tish
-            <i className="far fa-arrow-right"></i>
-          </button>
-        </form>
-
-        <div className="flex items-center justify-center flex-wrap gap-2 text-center mt-5">
-          <span>Hisob mavjudmi?</span>
-          <Link to="/sign-in" className="font-bold text-purple-400">
-            Tizimga kiring
-          </Link>
-        </div>
+    <div className="mx-auto bg-base-300 w-max max-w-full py-5 px-14 sm:my-10 sm:rounded">
+      <p className="text-3xl sm:text-4xl font-bold capitalize max-w-xs text-center my-5">
+        <span className="text-accent">Xush kelibsiz!</span>
+      </p>
+      <div className="flex justify-center mt-10 mb-5">
+        <OAuth />
       </div>
-    </>
+      <form className="form-control" onSubmit={onSubmit}>
+        <div className="form-control-magic">
+          <input
+            className="input input-ghost w-full"
+            type="text"
+            id="name"
+            value={name}
+            onChange={onChange}
+            required
+          />
+          <label className="label block" ref={ref1}>
+            Ism
+          </label>
+        </div>
+        <div className="form-control-magic">
+          <input
+            className="input input-ghost w-full"
+            type="email"
+            id="email"
+            value={email}
+            onChange={onChange}
+            required
+          />
+          <label className="label block" ref={ref2}>
+            Email
+          </label>
+        </div>
+        <div className="form-control-magic flex items-center justify-center">
+          <input
+            className="input input-ghost w-full"
+            type={showPassword ? 'text' : 'password'}
+            id="password"
+            value={password}
+            onChange={onChange}
+            required
+          />
+          <label className="label block" ref={ref3}>
+            <span className="label-text text-xl">Parol</span>
+          </label>
+          <button
+            type="button"
+            className="btn btn-outline btn-square btn-sm absolute right-2"
+            onClick={() => setShowPassword((prevState) => !prevState)}
+          >
+            <i className="far fa-eye"></i>
+          </button>
+        </div>
+
+        <button className="btn btn-block btn-primary mt-5 gap-3">
+          Ro'yxatdan o'tish
+          <i className="far fa-arrow-right"></i>
+        </button>
+      </form>
+
+      <div className="flex items-center justify-center flex-wrap gap-2 text-center mt-5">
+        <span>Hisob mavjudmi?</span>
+        <Link to="/sign-in" className="font-bold text-purple-400">
+          Tizimga kiring
+        </Link>
+      </div>
+    </div>
   )
 }
 
